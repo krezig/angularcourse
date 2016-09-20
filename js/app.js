b40e40a1ac3a51ddabfd792fa9ba9db04207f623
krezig <eseduardo14@gmail.com>
2016-09-20 12:28:59 -0600

    Archivo Subido

---
+++ js/app.js
@@ -0,0 +1,106 @@
+(function(){
+
+	var gem = {
+		name: 'Azurite',
+		price: '2.95',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+		canPurchase: false,
+		soldOut: true
+	};
+
+	var gems = [
+		{
+			name: 'Azurite',
+			price: '2.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-01.gif'},
+				{id:1, url:'img/gem-02.gif'},
+				{id:2, url:'img/gem-03.gif'}
+			],
+			reviews: [
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'},
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'}
+			]
+		},
+		{
+			name: 'Bloodstone',
+			price: '5.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-04.gif'},
+				{id:1, url:'img/gem-05.gif'},
+				{id:2, url:'img/gem-06.gif'}
+			],
+			reviews: [
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'},
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'}
+			]
+		},
+		{
+			name: 'Zircon',
+			price: '3.95',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ipsum, iusto voluptates laudantium quam et repellendus facilis tenetur quasi ea, doloremque veritatis velit in, laboriosam quibusdam vero incidunt ipsam voluptatum.',
+			canPurchase: false,
+			soldOut: true,
+			images: [
+				{id:0, url:'img/gem-07.gif'},
+				{id:1, url:'img/gem-08.gif'},
+				{id:2, url:'img/gem-09.gif'}
+			],
+			reviews: [
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'},
+				{stars: 5, body: 'I love this product', author:'alonso@gmail.com'}
+			]
+		}	
+	];
+
+	var app = angular.module('gemStore', ['store-products']);
+
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
+	app.controller('ReviewsController',function(){
+      this.review = {};
+      this.addReview = function(product){
+      	product.reviews.push(this.review);
+      	this.review = {};
+      }
+
+	});
+
+	app.controller('GalleryController', function(){
+		this.current = 0;
+		this.setCurrent = function(id){
+			this.current = id || 0;
+		}
+	});
+
+	/*app.directive('productTitle', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'product-title.html'
+		};
+	});
+*/
+
+
+
+})();
